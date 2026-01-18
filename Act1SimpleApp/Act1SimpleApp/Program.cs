6fa2c0fad342b7b48ae7ef37d13fcf8787ef7cc4
ssmnthmc <ssmnthmc24@gmail.com>
2026-01-19 00:29:01 +0800

    activity 1 simple c# console application

---
+++ Act1SimpleApp/Act1SimpleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿//Salgado, Samantha Marion C. - Activity 1 - Simple C# Console Application
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> tasks = new List<string>();
+
+        while (true)
+        {
+            Console.WriteLine("\n=== TO-DO LIST ===");
+            Console.WriteLine("1 - Add task");
+            Console.WriteLine("2 - View tasks");
+            Console.WriteLine("3 - Remove task");
+            Console.WriteLine("4 - Exit");
+            Console.Write("\nEnter a number: ");
+
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Write("\nEnter task: ");
+                tasks.Add(Console.ReadLine());
+                Console.Write("\nTask Recorded!\n");
+            }
+            else if (choice == "2")
+            {
+                Console.Write("\nTask List: \n");
+                for (int i = 0; i < tasks.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {tasks[i]}");
+                }
+            }
+            else if (choice == "3")
+            {
+                Console.Write("\nEnter task number: ");
+                int index = int.Parse(Console.ReadLine());
+                tasks.RemoveAt(index - 1);
+                Console.Write("\nTask Removed!\n");
+            }
+            else if (choice == "4")
+            {
+                break;
+            }
+        }
+    }
+}
+
