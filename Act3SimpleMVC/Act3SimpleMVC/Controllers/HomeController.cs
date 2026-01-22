0ccb4d7d06f18e3e86779241e6ca4721453ceda1
ssmnthmc <ssmnthmc24@gmail.com>
2026-01-23 00:19:37 +0800

    act 3 simple mvc

---
+++ Act3SimpleMVC/Act3SimpleMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Act3SimpleMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Act3SimpleMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Act3SimpleMVC/Act3SimpleMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Act3SimpleMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
